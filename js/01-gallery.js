5f442c0bdf8fcdfc48f07704bf3eca3fdbc0f92e
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-04-25 20:32:40 +0200

    Create ImageGallery - js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const createGalleryMarkup = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+createGalleryMarkup.insertAdjacentHTML('beforeend', galleryMarkup);
+createGalleryMarkup.addEventListener('click', onGalleryClick)
+
+
+function createGalleryItems(items) {
+    return items
+        .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+        })
+    .join('')
+};
+
+function onGalleryClick(e) {
+    const isGalleryActive = e.target.classList.contains('gallery__link');
+
+    if (!isGalleryActive) {
+        return;
+    }  
+    e.prevenDefault();
+
+    if (e.code === 'Escape') {
+        lightbox.close();  //разобраться в этом.
+    }
+}
+
+
 
 console.log(galleryItems);

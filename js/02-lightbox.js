d31088d9faa30078d725344283142f91639ff329
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-04-26 14:42:13 +0200

    Create task-2 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const createGalleryItems = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+createGalleryItems.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+    return items
+        .map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    })
+    .join('')
+
+}
+
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    docClose: false
+
+ });
+
+lightbox.on('show.simplelightbox', (e) => e.preventDefault());
+
+
+
+
+
+
+
 console.log(galleryItems);
